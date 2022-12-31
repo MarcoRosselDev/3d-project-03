1c23995f5af3ede9460e401e33a5447a37c09130
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-30 23:08:58 -0300

    handleRingColor function

--- src/components/Properties/Options/MetalColor/MetalColor.js
+++ src/components/Properties/Options/MetalColor/MetalColor.js
@@ -5,13 +5,17 @@ import {
 } from "./MetalColor.elements";
 
 const MetalColor = ({ setCurrentRingColor }) => {
+  const handleRingColor = (ringColor) => {
+    setCurrentRingColor({ color: ringColor });
+  };
+
   return (
     <OptionMetalColorContainer>
       <h2>Metal Color Options</h2>
       <OptionMetalColorDetails>
-        <h4>Gold</h4>
-        <h4>Silver</h4>
-        <h4>Copper</h4>
+        <h4 onClick={() => handleRingColor("#DAA520")}>Gold</h4>
+        <h4 onClick={() => handleRingColor("#ffffff")}>Silver</h4>
+        <h4 onClick={() => handleRingColor("#B87333")}>Copper</h4>
       </OptionMetalColorDetails>
     </OptionMetalColorContainer>
   );
