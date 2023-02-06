099748b764fab4c781ce9bfdfa1999bf13f265ff
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-06 14:53:46 -0300

    delete reminder

--- src/components/reminder/app.js
+++
@@ -1,7 +0,0 @@
-const myInput = document.querySelector(".myInput");
-const pushButton = document.querySelector(".pushButton");
-const container = document.querySelector(".container");
-
-pushButton.addEventListener("click", function () {
-  container.innerHTML = myInput.value;
-});
